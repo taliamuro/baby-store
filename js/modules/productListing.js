95c14e5b4d787f2fcb8be67ee094194585c9591b
taliamuro <taliamuro3@gmail.com>
2025-04-21 14:10:10 -0400

    Added the logo and styled the search button

--- js/modules/productListing.js
+++ js/modules/productListing.js
@@ -1,1 +1,19 @@
 // ES Module for implementing the product listing logic
+export async function fetchShows() {
+    console.log("Fetching products...");
+    const uri = "https://api.escuelajs.co/api/v1/products";
+    const products = await fetchProducts(uri);
+}
+
+export function parseProducts(products) {
+    products.array.forEach(product => {
+        
+    });
+}
+
+function createNewElement(parent, elemName, content) {
+    const newElem = document.createElement(elemName);
+    newElem.textContent = content;
+    parent.appendChiled(newElem);
+    return newElem;
+}
