12fd801f169a6d53380562df3f77acc973275fa1
taliamuro <taliamuro3@gmail.com>
2025-04-22 17:19:03 -0400

    Added flex styling to the cards

--- js/modules/productListing.js
+++ js/modules/productListing.js
@@ -39,8 +39,8 @@ export function parseProducts(products) {
         const imageSrc = (product.images && product.images.length > 0) ? product.images[0] : "images/placeholder-image.jpeg";
 
         card.innerHTML = `
-            <img src="${product["thumbnail_img"] || 'https://via.placeholder.com/300x200'}" alt="${product["item-title"]}" height="250" width="190">
-            <h3>${product["item_title"]}</h3>
+            <img src="${product["thumbnail_img"] || 'https://via.placeholder.com/300x200'}" alt="${product["item-title"]}" height="350">
+            <h3>${product["item_title"].length > 15 ? product["item_title"].substring(0,15) + '...' : product["item_title"]}</h3>
             <p>$${product["unit_price"]}</p>
         `;
 
