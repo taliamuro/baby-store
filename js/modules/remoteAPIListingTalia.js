f56dd75241afb98f34542e76740aa6e56af8cd10
taliamuro <taliamuro3@gmail.com>
2025-05-19 12:01:41 -0400

    Finished my fetch API

--- js/modules/remoteAPIListingTalia.js
+++ js/modules/remoteAPIListingTalia.js
@@ -7,8 +7,9 @@ export function initRemoteAPIListingTalia(params) {
 export async function fetchProductsfromAPI() {
     console.log("Fetching products from API...");
     try {
-        const uri = "https://api.escuelajs.co/api/v1/products";
-        const products = await fetchData(uri);
+        const uri = "https://dummyjson.com/products";
+        const data = await fetchData(uri);
+        const products = data.products;
         parseProducts(products);
     } catch (error) {
         console.error("Error loading products: ", error);
@@ -19,16 +20,15 @@ export function parseProducts(products) {
     console.log(products);
     const container = document.getElementById("product-container");
     
-    products.forEach((product, index) => {
+    products.forEach((product) => {
         const card = document.createElement("div");//add a section
         card.className = "product-card";//We need a class name to style it
         
         const imageSrc = "images/placeholder-image.jpeg";//Adding placeholder image
         
         card.innerHTML = `
-            <img src="${product.images[1] || 'https://via.placeholder.com/300x200'}" alt="${product.title}" height="350">
-            <h5><a class="listing-description-preview" href="product-details.html?id=${index}">${product.title.length > 20 ? product.title.substring(0,20) + '...' : product.title}</h5>
-            <p>$${product.price}</p>
+            <img src="${product.images[0] || 'https://via.placeholder.com/300x200'}" alt="${product.title}" height="350">
+            <h5><a class="listing-description-preview" id="${product["id"]}">${product["title"]}</a></h5>
         `;
 
         container.appendChild(card);
