1487dae2d610b82c4f6bda1aa10aff96882c7ac0
markololo <markololo2468@gmail.com>
2025-05-20 00:57:10 -0400

    tried to resize carousel and made the carousel turn every 4 seconds

--- js/modules/home.js
+++ js/modules/home.js
@@ -1,5 +1,11 @@
 // ES Module for implementing the home logic
-
 export function initHome() {
     
+    const myCarouselElement = document.querySelector('#carouselAutoplaying')
+
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+        interval: 4000
+    });
+
+
 }
