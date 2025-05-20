0cae68075be0d4d8fee8fa8fb580d601729b224b
taliamuro <taliamuro3@gmail.com>
2025-05-20 16:07:12 -0400

    Changed all home.html to index.html

--- js/modules/orderConfirmation.js
+++ js/modules/orderConfirmation.js
@@ -10,44 +10,6 @@ export function initOrderConfirmation()
             localStorage.removeItem("show-confetti");
         }
     });
-    
-    // document.addEventListener("DOMContentLoaded", () => {
-    //     const showConfetti = localStorage.getItem("show-confetti");
-
-    //     // if (showConfetti === "true") {
-    //     if (localStorage.getItem("show-confetti") === "true") {
-    //         console.log("Firing confetti...");
-    //         const defaults = {
-    //             spread: 360,
-    //             ticks: 100,
-    //             gravity: 0,
-    //             decay: 0.94,
-    //             startVelocity: 30,
-    //             shapes: ["heart"],
-    //             colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
-    //         };
-
-    //         confetti({
-    //         ...defaults,
-    //         particleCount: 50,
-    //         scalar: 2,
-    //         });
-
-    //         confetti({
-    //         ...defaults,
-    //         particleCount: 25,
-    //         scalar: 3,
-    //         });
-
-    //         confetti({
-    //         ...defaults,
-    //         particleCount: 10,
-    //         scalar: 4,
-    //         });
-    //         // fireConfetti();
-    //         localStorage.removeItem("show-confetti");
-    //     }
-    // });    
 }
 
 function fireConfetti() {
